1caddf62fa48156816811186b6499c9b01c6ac04
Jesse Monroy <jessemonroy650@yahoo.com>
2015-05-23 22:54:59 -0700

    Add video support and some minor changes

--- camera.js
+++ camera.js
@@ -1,7 +1,13 @@
 //
+//	2015-05-23 - Added Video
+//	https://github.com/apache/cordova-plugin-camera/
 //
-//
-var cameraOptions = { quality: 50, destinationType: Camera.DestinationType.FILE_URI };
+var cameraOptions = {
+	quality: 50,
+	destinationType: Camera.DestinationType.FILE_URI,
+	sourceType: PHOTOLIBRARY, // This is required with, mediaType: VIDEO 
+	mediaType: VIDEO
+ };
 
 function getCamera() {
 	navigator.camera.getPicture(onSuccessCamera, onFailCamera, cameraOptions);
@@ -9,7 +15,12 @@ function getCamera() {
 
 function onSuccessCamera(imageURI) {
     var image = document.getElementById('myImage');
+	// place image in view
     image.src = imageURI;
+	// reuse object
+	image = document.getElementById('imgName');
+	// write the text next to the image
+    image.innerHTML = imageURI;
 }
 
 function onFailCamera(message) {
